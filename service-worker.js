d6264401e6d52e272810757e63a988c2f4c2edbe
unknown <roee1454@gmail.com>
2023-09-16 21:55:10 +0300

    u

--- service-worker.js
+++ service-worker.js
@@ -2,6 +2,8 @@ self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open("my-pwa-cache").then((cache) => {
       return cache.addAll(["/"]);
+    }).catch(err => {
+        console.error(err);
     })
   );
 });
@@ -10,6 +12,8 @@ self.addEventListener("fetch", (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => {
       return response || fetch(e.request);
+    }).catch(err => {
+        console.error(err);
     })
   );
 });
